6d8d9a4e3a0cbc78da00af333800c3e565950057
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-01-27 13:37:00 -0300

    download_one_day function

--- embrace.py
+++ embrace.py
@@ -1,10 +1,10 @@
-from common import request, download
-from base import date_from_filename
+# embrace_utils import fname_attrs
+from base import request, download
 import os
-import datetime
+import datetime as dt
 import pandas as pd
 
-infos = {
+site_code = {
     
          "ionosonde": {"Fortaleza": "FZA0M", 
                        "Sao luis": "SAA0K", 
@@ -25,31 +25,39 @@ infos = {
 
 
 def URL(date, 
-        instrument = "imager", 
-        site = "Cariri"):
+        site = "Cariri", 
+        inst = "imager"):
     
-    """Build embrace data download from day and year"""
+    """
+    Build embrace url from date, site 
+    for an intrument
+    """
     url = "https://embracedata.inpe.br/"
     
-    code = infos[instrument][site]
+    code = site_code[inst][site]
     
     year = date.year
     str_doy = date.strftime("%j")
     str_mon = date.strftime("%m")
     str_day = date.strftime("%d")
     
-    if instrument == "imager":
-        url += f"{instrument}/{code}/{year}/{code}_{year}_{str_mon}{str_day}/"
+    url += f"{inst}/{code}/{year}/"
+    
+    if inst == "imager":
+        url += f"{code}_{year}_{str_mon}{str_day}/"
         
-    elif instrument == "ionosonde":
-        url += f"{instrument}/{code}/{year}/{str_doy}/"
+    elif inst == "ionosonde":
+        url += f"{str_doy}/"
+        
+    elif inst == "magnetometer":
+        url +=  f"{code}/" # NOT COMPLETED
     
     return url
 
 
 
 
-def get_hiperlinks(date, 
+def filter_hiperlinks(date, 
                    instrument = "imager", 
                    site = "Cariri", 
                    down = False,
@@ -57,53 +65,81 @@ def get_hiperlinks(date,
     
     """Get urls for the date input"""
     
-    delta = datetime.timedelta(days = 1)
+    delta = dt.timedelta(days = 1)
     
     times = pd.date_range(f"{date} 21:00", 
                           f"{date + delta} 07:00", 
                           freq = "10min")
+    links = []
     
     for date in [date, date + delta]:
         url = URL(date, instrument = instrument, site = site)
-        links = request(url, times)
+        links.append(request(url))
     
-        if down:
-            for link in links:
-                print("downloading...", link)
-                download(url, link, path_to_save = os.path.join(path_dst, 
-                                                     instrument))
-
+    return links
     
 
 
-def filter_links():
+class href_attrs(object):
     
-    links = request(URL(date))
+    def __init__(self, file):
+        
+        self.drift = ["SKY", "DFT", "DVL"]
+        self.ionog = ["RSF", "SAO", "PNG"]
+        
+        if any(file.endswith(f) for f in 
+               self.drift + self.ionog):
+            self.datetime = self.iono(file)
+        else:
+            pass
+            
+        
+    @staticmethod
+    def iono(file):
+        
+        args = file[:-4].split("_")
+    
+        year = int(args[1][:4])
+        doy = int(args[1][4:7])
+        hour = int(args[1][7:9])
+        minute = int(args[1][9:11])
+        second = int(args[1][11:])
+        date = (dt.date(year, 1, 1) + 
+                dt.timedelta(doy - 1))
+    
+        day = date.day
+        month = date.month
+    
+        return dt.datetime(year, 
+                           month, 
+                           day,
+                           hour, 
+                           minute, 
+                           second)
+    
+    
 
 
+date = dt.datetime(2013, 1, 1)
+
+def download_one_day(date, 
+                      site = "Sao Luis", 
+                      inst = "ionosonde"):
+    url = URL(date, 
+              site = site, 
+              inst = inst)
+    
+    links = request(url)
+        
     for link in links:
-        
-    if "imager" in url:
-        cond = ["O6" in href, "DARK" not in href]
-
-    elif "ionosonde" in url:
-        
-        cond = [".RSF" in href]
-        
-    if all(cond):
-        ref_time = date_from_filename(href).datetime
-        if (ref_time >= times[0]) and (ref_time <= times[-1]):
-            out_href.append(href)
-            
-            
-date = datetime.date(2014, 1, 3)
-
-
-
     
+        attrs = href_attrs(link)
     
-
-
+        if any(link.endswith(f) for f in attrs.drift):
+            
+            print("downloading...", attrs.datetime)
+            
+            download(url, link)
 
 
 
