c80334c2e2afe111b583e7e7a59ca679d80ab896
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-01-29 17:12:49 -0300

    download_one_year and build_dir funcs

--- embrace.py
+++ embrace.py
@@ -1,28 +1,8 @@
-# embrace_utils import fname_attrs
-from base import request, download
+from embrace_utils import href_attrs, site_codes
+from core import request, download
 import os
-import datetime as dt
 import pandas as pd
-import pandas as pd
-
-site_code = {
-    
-         "ionosonde": {"Fortaleza": "FZA0M", 
-                       "Sao luis": "SAA0K", 
-                       "Belem": "BLJ03", 
-                       "Cachoeira": "CAJ2M", 
-                       "Santa Maria": "SMK29", 
-                       "Boa Vista": "BVJ03", 
-                       "Campo Grande": "CGK21"}, 
-         
-         "imager": {"Cariri": "CA", 
-                    "Bom Jesus da Lapa" : "BJL", 
-                    "Cachoeira Paulista": "CP", 
-                    "Comandante Ferraz": "CF", 
-                    "Sao Martinho da Serra": "SMS"} 
-         }
-
-
+from pathlib import Path
 
 
 def URL(date, 
@@ -35,7 +15,7 @@ def URL(date,
     """
     url = "https://embracedata.inpe.br/"
     
-    code = site_code[inst][site]
+    code = site_codes[inst][site]
     
     year = date.year
     str_doy = date.strftime("%j")
@@ -55,74 +35,12 @@ def URL(date,
     
     return url
 
-
-
-
-def filter_hiperlinks(date, 
-                   instrument = "imager", 
-                   site = "Cariri", 
-                   down = False,
-                   path_dst = ""):
     
-    """Get urls for the date input"""
-    
-    delta = dt.timedelta(days = 1)
-    
-    times = pd.date_range(f"{date} 21:00", 
-                          f"{date + delta} 07:00", 
-                          freq = "10min")
-    links = []
-    
-    for date in [date, date + delta]:
-        url = URL(date, instrument = instrument, site = site)
-        links.append(request(url))
-    
-    return links
-    
-
-
-class href_attrs(object):
-    
-    def __init__(self, file):
-        
-        self.drift = [ "DVL"] #"SKY", "DFT",
-        self.ionog = ["RSF", "SAO", "PNG"]
-        
-        if any(file.endswith(f) for f in 
-               self.drift + self.ionog):
-            self.datetime = self.iono(file)
-        else:
-            pass
-            
-        
-    @staticmethod
-    def iono(file):
-        
-        args = file[:-4].split("_")
-    
-        year = int(args[1][:4])
-        doy = int(args[1][4:7])
-        hour = int(args[1][7:9])
-        minute = int(args[1][9:11])
-        second = int(args[1][11:])
-        date = (dt.date(year, 1, 1) + 
-                dt.timedelta(doy - 1))
-    
-        day = date.day
-        month = date.month
-    
-        return dt.datetime(year, 
-                           month, 
-                           day,
-                           hour, 
-                           minute, 
-                           second)
-    
-
 def download_one_day(date, 
                       site = "Sao luis", 
                       inst = "ionosonde", 
-                      infile_save = ""):
+                      save_in = "", 
+                      ext = ["DVL"]):
     url = URL(date, 
               site = site, 
               inst = inst)
@@ -133,43 +51,93 @@ def download_one_day(date,
     
         attrs = href_attrs(link)
     
-        if any(link.endswith(f) for f in attrs.drift):
-            
+        if any(link.endswith(f) for f in ext):
             print("downloading...", attrs.datetime)
+            download(url, link, save_in)
             
-            download(url, link, infile_save)
+    return url
 
-def run_for_all_year(site = "Sao luis", 
-                     inst = "ionosonde",
-                     year = 2015):
+def download_all_year(inst, site, year, root):
     
+    d = build_dir(inst, site, year, root)
+
+    d.site_path
+    d.year_path
+
     dates = pd.date_range(f"{year}-1-1", 
                           f"{year}-12-31", 
-                          freq= "1D")
-    
-
-    root = f"D:\\drift\\FZA\\{year}\\"
-    
-    for date in dates:
-        
+                          freq = "1D")
+    for date in dates:    
         doy_str = date.strftime("%j")
-        path_to_create = os.path.join(root, doy_str)
+        save_in = d.doy_path(doy_str)
         
         try:
-            os.mkdir(path_to_create)
-        except:
-            pass
-        
-        try:
-            download_one_day(date, 
-                             site = site, 
-                            inst = inst, 
-                            infile_save = path_to_create)
+
+            download_one_day(
+                date, 
+                site = site, 
+                inst = inst, 
+                infile_save = save_in)
         except:
             continue
 
-run_for_all_year(year = 2015)
 
 
+class build_dir(object):
     
+    """
+    Create directories from input names:
+        inst: intrumentation type (e.g. imager)
+        site: site location for each inst.
+        year: observation year
+        root: path root
+    """
     
+    def __init__(
+            self, inst, site, year, root,
+             ):
+  
+        self.root = str(root)
+        self.year = str(year)
+        self.name_dir = site_codes[inst][site][:3]
+        
+    @staticmethod
+    def create_dir(path):
+        try:
+            os.mkdir(path)
+        except OSError:
+            print(f"{path} wasn`t created")         
+        return path
+        
+    @property  
+    def site_path(self):
+        return self.create_dir(
+                    os.path.join(
+                    self.root, 
+                    self.name_dir))
+        
+    @property
+    def year_path(self): 
+        return self.create_dir(
+                    os.path.join(
+                    self.root, 
+                    self.name_dir, 
+                    self.year))
+    
+    def doy_path(self, doy): 
+        return self.create_dir(
+                    os.path.join(
+                    self.root, 
+                    self.name_dir, 
+                    self.year, 
+                    str(doy)))
+    
+   
+def main():
+
+    root = "D:\\drift\\"
+    root = str(Path.cwd())
+    inst = "ionosonde"
+    site = "Fortaleza"
+    year = 2015
+    download_all_year(inst, site, year, root)
