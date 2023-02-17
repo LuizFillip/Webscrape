4273d7f2b85f99d011df2f39abd7b6111245d594
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-17 12:06:13 -0300

    updating functions

--- core.py
+++ core.py
@@ -1,16 +1,15 @@
 import os
 import requests 
 from bs4 import BeautifulSoup 
-import typing as T
 
 
 def download(url: str, href: str, infile_save: str = ""):
     
-    """Use request for download the data"""
+    """Function for download from link"""
     
     remote_file = requests.get(url + href)
     path_to_save = os.path.join(infile_save, href)
-    
+    print("download...", href)
     with open(path_to_save, 'wb') as f:
         for chunk in remote_file.iter_content(chunk_size = 1024): 
             if chunk: 
@@ -19,15 +18,35 @@ def download(url: str, href: str, infile_save: str = ""):
     return path_to_save
 
 
-def request(url):
-    
-    """Request url and filter times"""
+def request(url): ## condition for string in hiperlink
+    """Request website from url (RINEX or sp3)"""
     r = requests.get(url)
     s = BeautifulSoup(r.text, "html.parser")
- 
+
     parser = s.find_all('a', href = True)
-    
 
     return [link['href'] for link in parser]
 
+
+
+def make_dir(date, root = ""):
+    """Create a new directory by path must be there year and doy"""
+    doy_str = date.strftime("%j")
+    
+    path_to_create = os.path.join(root, doy_str)
+    
+    
+    try:
+        os.mkdir(path_to_create)
+        
+        for subfolder in ["imager", "ionosonde", "tec"]:   
+            os.makedirs(os.path.join(path_to_create, subfolder)) 
+        
+            print(f"Creation of the directory {subfolder} successfully")
+        
+    except OSError:
+        print(f"Creation of the directory {path_to_create} failed")
+      
+   
+    return path_to_create
 
