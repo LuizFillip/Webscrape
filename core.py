5797a93c04ecb6382806bd239953566fc11df1f5
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-07-04 22:57:30 -0300

    verify kword on request

--- core.py
+++ core.py
@@ -3,13 +3,21 @@ import requests
 from bs4 import BeautifulSoup 
 
 
-def download(url: str, href: str, infile_save: str = "") -> str:
+def download(url: str, href: str, 
+             infile_save: str = "", 
+             verify = False) -> str:
     
     """Function for download from link"""
     
-    remote_file = requests.get(url + href)
+    remote_file = requests.get(
+        url + href, 
+        verify = verify
+        )
+    
     path_to_save = os.path.join(infile_save, href)
+    
     print("download...", href)
+    
     with open(path_to_save, 'wb') as f:
         for chunk in remote_file.iter_content(
                 chunk_size = 1024
@@ -20,10 +28,11 @@ def download(url: str, href: str, infile_save: str = "") -> str:
     return path_to_save
 
 
-def request(url) -> list: 
+def request(url, verify = False) -> list: 
     
-    """Request website from url"""
-    r = requests.get(url)
+    """Request website code source from url"""
+    
+    r = requests.get(url, verify = verify)
     s = BeautifulSoup(r.text, "html.parser")
 
     parser = s.find_all('a', href = True)
