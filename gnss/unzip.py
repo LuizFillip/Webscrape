090a38d4bf8070802196d23be352a3711c123dcb
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-08-19 14:36:02 -0300

    moving from core

--- gnss/unzip.py
+++ gnss/unzip.py
@@ -1,6 +1,50 @@
 import gzip
 import shutil
 import os
+import zipfile
+from unlzw3 import unlzw
+
+def unzip_orbit(files): 
+    fh = open(files, 'rb')
+    
+    
+    compressed_data = fh.read()
+    
+    uncompressed_data = unlzw(compressed_data)
+    
+    str_mybytes = str(uncompressed_data)
+    
+    decoded = eval(str_mybytes).decode('utf8')
+    
+    file = open(files.replace(".Z", ""), 'w')
+    file.write(decoded)
+    file.close()
+    fh.close()
+    os.remove(files)
+    
+    
+def unzip_rinex(
+        files:str, 
+        year:int, 
+        path_to_save:str
+        ) -> None:
+    
+    zip_path = os.path.join(path_to_save, files)
+    zip_file = zipfile.ZipFile(zip_path, 'r') 
+    ext_year = str(year)[-2:] 
+    
+    extensions = [f"{ext_year}o", f"{ext_year}d"]
+    
+    zip_file = zipfile.ZipFile(zip_path, 'r') 
+    
+    for file in zip_file.namelist():
+        
+        if any(file.endswith(ext) for ext in extensions):
+            
+            zip_file.extract(file, path_to_save)
+            
+    zip_file.close()
+    os.remove(zip_path)
 
 def unzip_gz(infile):
     
