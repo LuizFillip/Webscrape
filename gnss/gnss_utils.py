9e3648fffafe0d6aff9c4cae3c53a44bf7d1d6ca
Luiz Fillip <72110299+LuizFillip@users.noreply.github.com>
2025-02-12 07:37:27 -0300

    Delete gnss/gnss_utils.py

--- gnss/gnss_utils.py
+++
@@ -1,9 +0,0 @@
-
-
-
-ibge_stations= ['amco', 'amcr', 'amha', 'ampt', 'amte',
-                'amua', 'aplj', 'apma', 'aps1', 'bele', 
-                'bepa', 'impz', 'maba', 'mabb', 'mtji', 
-                'naus', 'paar', 'pait', 'pasm', 'pove', 
-                'riob', 'rogm', 'roji', 'salu', 'cruz', 
-                'rogu', 'rovi', 'ceeu', 'ceft', 'rnna', 'pbjp']
